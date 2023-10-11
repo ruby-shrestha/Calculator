a0ce89dfa44333078bb4f77b63015868ed7214a7
ruby-shrestha <113083799+ruby-shrestha@users.noreply.github.com>
2023-10-11 10:33:31 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let scrn=document.getElementById('screen');
+
+function input(x){
+    scrn.value=scrn.value+x;
+}
+
+function allclear()
+{
+    scrn.value=' ';
+}
+
+function del()
+{
+    scrn.value=scrn.value.slice(0,-1);
+}
+
+function plusminus()
+{
+    scrn.value= - scrn.value;
+}
+function calculation()
+{
+    try {
+        scrn.value=eval(scrn.value);
+    }
+    catch{
+        scrn.value='Error';
+    }
+
+}
